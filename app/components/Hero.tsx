40f0b7374ab5b8b177ca6b8a7ccd83f7be6c151c
dominiclye <dominiclye91@gmail.com>
2024-09-20 00:04:56 +1000

    feat: Hero section

---
+++ app/components/Hero.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import React from 'react'
+import { AuroraBackground } from './ui/aurora-background';
+import { motion } from 'framer-motion';
+import { FlipWords } from './ui/flip-words';
+
+const Hero = () => {
+  return (
+    <div>
+      <AuroraBackground>
+        <motion.div
+          initial={{ opacity: 0.0, y: 40 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          className="relative flex flex-col gap-4 items-center justify-center px-4"
+        >
+          <h1 className='text-8xl'>🫖</h1>
+          <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
+            Hello, I'm Dominic.
+          </div>
+          <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4 overflow-hidden pl-10">
+            <span>an avid</span>
+            <div className="relative inline-block min-w-[11ch]">
+              <FlipWords words={['Snowboarder', 'Developer', 'Student']} />
+            </div>
+          </div>
+        </motion.div>
+      </AuroraBackground>
+    </div>
+  );
+}
+
+export default Hero
---
+++ app/components/Projects.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div className='bg-zinc-900 flex flex-col items-center min-h-screen'>
+        <h1 className='mt-5 font-bold text-5xl dark:text-white'>Projects</h1>
+    </div>
+  )
+}
+
+export default Projects
