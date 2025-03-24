8134a33cb262c3dec86e3f564a7fd5c7de0de9c1
dominiclye <dom27lye@gmail.com>
2025-03-24 15:20:26 +1100

    About me section

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -51,7 +51,12 @@ export default function AboutMe() {
         </div>
         <div className='p-5 [font-family:var(--font-custom)] flex flex-col gap-y-5'>
             <h1 className='text-6xl'>About Me</h1>
-            <p className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. At eos nihil perspiciatis nam? Voluptas, quisquam minus? Sed blanditiis, quaerat dolor doloribus ullam at voluptatum fugit, delectus voluptatibus nulla dolorem distinctio!</p>
+            <p className=''>I&apos;m a 19-year-old software engineer focused on building robust, efficient solutions for backend systems and machine learning applications.
+I began coding at 14 by creating small Discord bots for my friends, which gradually evolved into a more serious pursuit. During the COVID lockdowns, I ran a Discord-based database search engine that served over 200 users — a project that became my primary source of income before I was eligible for employment.<br /><br />
+
+Around the time I picked up Go, I began experimenting with reverse engineering TikTok&apos;s mobile endpoints. Go&apos;s lightweight concurrency model and network performance, especially when paired with fasthttp, enabled me to develop an autoclaimer that “claimed” rare usernames the moment they became available.<br /><br />
+
+While backend engineering remains my core strength, I picked up enough web development to bring my ideas into production, and recently began exploring WebGL and Three.js as a creative outlet. Since starting university, I&apos;ve turned my focus toward deepening my understanding of data science, backend systems, and low-level programming — working primarily with Python, Go, and C.</p>
         </div>
     </div>
   )
